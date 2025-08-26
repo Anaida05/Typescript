ffef73a435e44fd1b637f37f708526dc23884aa1
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-08-26 17:43:58 +0530

    learnt the function,array and object

---
+++ practise.js
@@ -0,0 +1,13 @@
+//functions
+var greet = function (val) {
+    return "Hello," + val;
+};
+greet("anaida");
+//arrays
+var nums = [1, 2, 4];
+var names = ["anaida", "jim"];
+//objects
+var user = {
+    name: "Anaida",
+    age: 1
+};
---
+++ practise.ts
@@ -0,0 +1,16 @@
+//functions
+const greet = (val : string):string=>{
+    return "Hello," + val
+}
+
+greet("anaida")
+
+//arrays
+const nums : number[] = [1,2,4]
+const names : string[] = ["anaida","jim"]
+
+//objects
+const user :{name : string,age:number} = {
+    name : "Anaida",
+    age : 1
+}
