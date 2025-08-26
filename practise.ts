27de90bc74769812c4693c32b2402fa5e945e7ed
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-08-26 17:58:21 +0530

    interface and type alias

--- practise.ts
+++ practise.ts
@@ -14,3 +14,26 @@ const user :{name : string,age:number} = {
     name : "Anaida",
     age : 1
 }
+
+//tupe alias
+type Animal = {
+  species: string;
+  legs: number;
+};
+
+let dog: Animal = {
+  species: "Dog",
+  legs: 4
+};
+
+//interface
+interface Person {
+  name: string;
+  age: number;
+}
+
+let users: Person = {
+  name: "John",
+  age: 30
+};
+
